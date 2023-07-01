9201bd1a5883e82f3de530fbda230929ee30a214
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2023-07-01 14:12:09 +0300

    first commit

---
+++ src/main/java/dev/arif/blogbackend/BlogBackEndApplication.java
@@ -0,0 +1,12 @@
+package dev.arif.blogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackEndApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/arif/blogbackend/BlogBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arif.blogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
